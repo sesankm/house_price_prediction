a6ad28294904fb1c2a8589d5fbc2222a87a40253
sesankm <sesank.mallikarjuna@gmail.com>
2020-12-21 15:59:30 -0600

    scrape from homefinder.com

---
+++ scrape.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import requests
+
+cities = ["Houston", "Austin", "Dallas", "San-Antonio"]
+data = []
+
+for city in cities:
+    for i in range(1, 40):
+        if(i == 1):
+            resp = requests.get("https://homefinder.com/for-sale/TX/{}".format(city))
+        else:
+            resp = requests.get("https://homefinder.com/for-sale/TX/{}?page={}".format(city, i))
+        soup = bs(resp.text, "html.parser")
+        prices = [i.find('h4').text.replace("$", "").split() for i in soup.find_all("div", {"class":"attributes"})]
+        attributes = [i.text.split("|") for i in soup.find_all("div", {"class":"text-muted"})]
+        attributes = [[j.strip() for j in i] for i in attributes]
+        data += [i[:2] + j + [city] for i,j in zip(prices,attributes) if "Sale" in i and len(i) == 4]
+
+df = pd.DataFrame(data)
+df.columns = ["Price", "Type", "Bedrooms", "Bathrooms", "Square Feet", "City"]
+df.dropna(inplace=True)
+df[["Bedrooms", "Bathrooms", "Square Feet"]] = df[["Bedrooms", "Bathrooms", "Square Feet"]].applymap(lambda x: int(x.split()[0].replace(",", "")))
+df["Price"] = df["Price"].apply(lambda x: int(x.replace(",", "")))
+df.to_csv("house_prices.csv", index=False)
